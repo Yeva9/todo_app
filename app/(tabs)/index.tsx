06ee0174f360b8671699abe631429b182bd6c7c4
yeva <eva.hovnanyan.1999@gmail.com>
2026-03-24 23:20:46 +0400

    Made styles dynamic

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,38 +1,28 @@
-import { api } from "@/convex/_generated/api";
-import useTheme from "@/hooks/useTheme";
-import { useMutation, useQuery } from "convex/react";
+import useTheme, { ColorScheme } from "@/hooks/useTheme";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
-  const { toggleMode } = useTheme();
-
-  const todos = useQuery(api.todos.getTodos);
-  const addTodo = useMutation(api.todos.addTodo);
-  const clearTodos = useMutation(api.todos.clearAllTodos);
-  console.log(todos);
-
+  const { toggleMode, colors } = useTheme();
+  const styles = createStyles(colors);
+  
   return (
     <View style={styles.container}>
       <Text>Hello, Expo!</Text>
       <TouchableOpacity onPress={toggleMode}>
         <Text>Toggle The Mode</Text>
       </TouchableOpacity>
-
-      <TouchableOpacity onPress={() => addTodo({ text: "New Todo" })}>
-        <Text>Add Todo</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={() => clearTodos()}>
-        <Text>Clear All Todos</Text>
-      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-});
+const createStyles = (colors: ColorScheme) => {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: colors.bg,
+    },
+  });
+return styles;
+}
