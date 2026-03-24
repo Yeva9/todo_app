edc980422d0b30c0b7e2989327113971ecd9bb67
yeva <eva.hovnanyan.1999@gmail.com>
2026-03-24 23:09:52 +0400

    Integrated methods

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,29 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const { toggleMode } = useTheme();
+
+  const todos = useQuery(api.todos.getTodos);
+  const addTodo = useMutation(api.todos.addTodo);
+  const clearTodos = useMutation(api.todos.clearAllTodos);
+  console.log(todos);
+
   return (
     <View style={styles.container}>
       <Text>Hello, Expo!</Text>
       <TouchableOpacity onPress={toggleMode}>
         <Text>Toggle The Mode</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => addTodo({ text: "New Todo" })}>
+        <Text>Add Todo</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => clearTodos()}>
+        <Text>Clear All Todos</Text>
       </TouchableOpacity>
     </View>
   );
@@ -17,7 +33,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
-    // backgroundColor: "red",
+    alignItems: "center"
   },
 });
