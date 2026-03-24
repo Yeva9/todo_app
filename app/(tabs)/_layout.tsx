882b7d999cb028fbba6aeb27b649d74e86b8ef12
yeva <eva.hovnanyan.1999@gmail.com>
2026-03-24 22:02:43 +0400

    Added useTheme for Dark/Light mode

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,33 +1,35 @@
-// import useTheme from "@/hooks/useTheme";
+import useTheme from "@/hooks/useTheme";
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
 const TabsLayout = () => {
-  // const { colors } = useTheme();
+  const { colors } = useTheme();
 
   return (
-    <Tabs screenOptions={{
-      tabBarActiveTintColor: "red",
-      tabBarInactiveTintColor: "gray",
-      tabBarStyle: {
-        backgroundColor: 'pink',
-        borderTopWidth: 1,
-        borderTopColor: "yellow",
-        height: 90,
-        paddingBottom: 30,
-        paddingTop: 10,
-      },
-      tabBarLabelStyle: {
-        fontSize: 12,
-        fontWeight: "600",
-      },
-      headerShown: false,
-    }} >
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: colors.textMuted,
+        tabBarStyle: {
+          backgroundColor: colors.surface,
+          borderTopWidth: 1,
+          borderTopColor: colors.border,
+          height: 90,
+          paddingBottom: 30,
+          paddingTop: 10,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "600",
+        },
+        headerShown: false,
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: "Todos",
-          tabBarIcon: ({ color, size }) => <Ionicons name="flash-outline" size={size} color={color} />,
+          title: "Todos", // TODO: use from constants
+          tabBarIcon: ({ color, size }) => <Ionicons name="flash-outline" size={size} color={color} /> ,
         }}
       />
       <Tabs.Screen
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
+import useTheme from "@/hooks/useTheme";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const { toggleMode } = useTheme();
   return (
     <View style={styles.container}>
-      <Text>Hello, Expo1!</Text>
+      <Text>Hello, Expo!</Text>
+      <TouchableOpacity onPress={toggleMode}>
+        <Text>Toggle The Mode</Text>
+      </TouchableOpacity>
     </View>
   );
 }
